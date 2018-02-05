0d2e2cb3622b26df453dca93252a08a9071c0a1a
Andreas Bærentzen <janba@dtu.dk>
2018-02-05 18:21:06 +0100

    CMAKE now compiles GEL on mac. js.py was slightly modified to allow wireframe rendering of non triangle meshes.

--- src/PyGEL/js.py
+++ src/PyGEL/js.py
@@ -1,6 +1,6 @@
 """ PyGEL.js is a module with a single function, display, that provides functionality for displaying a mesh
     Manifold as an interactive 3D model in a Jupyter Notebook """
-import PyGEL as pgl
+from PyGEL import gel
 from numpy import array
 import plotly.offline as py
 import plotly.graph_objs as go
@@ -14,7 +14,9 @@ def display(m,wireframe=True,smooth=True,data=None):
         mesh is shown in a light grey color. If data contains an array of scalar values
         per vertex, these are mapped to colors used to color the mesh."""
     xyz = array([ p for p in m.positions()])
-    ijk = array([[ idx for idx in m.circulate_face(f,'v')] for f in m.faces()])
+    m_tri = gel.Manifold(m)
+    gel.triangulate(m_tri)
+    ijk = array([[ idx for idx in m_tri.circulate_face(f,'v')] for f in m_tri.faces()])
     mesh = go.Mesh3d(x=xyz[:,0],y=xyz[:,1],z=xyz[:,2],
             i=ijk[:,0],j=ijk[:,1],k=ijk[:,2],color='#dddddd',flatshading=not smooth)
     if data is not None:
