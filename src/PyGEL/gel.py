82326fcff9cc96d99866069f76462c67e4a7fc00
Andreas Bærentzen <janba@dtu.dk>
2018-09-13 14:13:20 +0200

    minor change to PyGEL so that it can work if there is no visual output

--- src/PyGEL/gel.py
+++ src/PyGEL/gel.py
@@ -704,82 +704,84 @@ def triangulate(m):
     splitting a polygon into smaller polygons. """
     lib_py_gel.shortest_edge_triangulate(m.obj)
 
-lib_py_gel.GLManifoldViewer_new.restype = ct.c_void_p
-lib_py_gel.GLManifoldViewer_delete.argtypes = (ct.c_void_p,)
-lib_py_gel.GLManifoldViewer_display.argtypes = (ct.c_void_p,ct.c_void_p,ct.c_char,ct.c_bool, ct.POINTER(ct.c_float*3), ct.POINTER(ct.c_double),ct.c_bool,ct.c_bool)
-lib_py_gel.GLManifoldViewer_get_annotation_points.restype = ct.c_size_t
-lib_py_gel.GLManifoldViewer_get_annotation_points.argtypes = (ct.c_void_p, ct.POINTER(ct.POINTER(ct.c_double)))
-lib_py_gel.GLManifoldViewer_set_annotation_points.argtypes = (ct.c_void_p, ct.c_int, ct.POINTER(ct.c_double))
-lib_py_gel.GLManifoldViewer_event_loop.argtypes = (ct.c_bool,)
-class GLManifoldViewer:
-    """ An OpenGL Viewer for Manifolds. Having created an instance of this
-    class, call display to show a mesh. The display function is flexible,
-    allowing several types of interactive visualization. Each instance of this
-    class corresponds to a single window, but you can have several
-    GLManifoldViewer and hence also several windows showing different
-    visualizations. """
-    def __init__(self):
-        self.obj = lib_py_gel.GLManifoldViewer_new()
-    def __del__(self):
-        lib_py_gel.GLManifoldViewer_delete(self.obj)
-    def display(self, m, mode='w', smooth=True, bg_col=[0.3,0.3,0.3], data=None, reset_view=False, once=False):
-        """ Display a mesh
+try:
+    lib_py_gel.GLManifoldViewer_new.restype = ct.c_void_p
+    lib_py_gel.GLManifoldViewer_delete.argtypes = (ct.c_void_p,)
+    lib_py_gel.GLManifoldViewer_display.argtypes = (ct.c_void_p,ct.c_void_p,ct.c_char,ct.c_bool, ct.POINTER(ct.c_float*3), ct.POINTER(ct.c_double),ct.c_bool,ct.c_bool)
+    lib_py_gel.GLManifoldViewer_get_annotation_points.restype = ct.c_size_t
+    lib_py_gel.GLManifoldViewer_get_annotation_points.argtypes = (ct.c_void_p, ct.POINTER(ct.POINTER(ct.c_double)))
+    lib_py_gel.GLManifoldViewer_set_annotation_points.argtypes = (ct.c_void_p, ct.c_int, ct.POINTER(ct.c_double))
+    lib_py_gel.GLManifoldViewer_event_loop.argtypes = (ct.c_bool,)
+    class GLManifoldViewer:
+        """ An OpenGL Viewer for Manifolds. Having created an instance of this
+        class, call display to show a mesh. The display function is flexible,
+        allowing several types of interactive visualization. Each instance of this
+        class corresponds to a single window, but you can have several
+        GLManifoldViewer and hence also several windows showing different
+        visualizations. """
+        def __init__(self):
+            self.obj = lib_py_gel.GLManifoldViewer_new()
+        def __del__(self):
+            lib_py_gel.GLManifoldViewer_delete(self.obj)
+        def display(self, m, mode='w', smooth=True, bg_col=[0.3,0.3,0.3], data=None, reset_view=False, once=False):
+            """ Display a mesh
 
-        Args:
-        ---
-        - m : the Manifold mesh we are visualizing.
-        - mode : a single character that determines how the mesh is visualized:
-            'w' - wireframe,
-            'i' - isophote,
-            'g' - glazed (try it and see),
-            's' - scalar field,
-            'l' - line field,
-            'n' - normal.
-        - smooth : if True we use vertex normals. Otherwise, face normals.
-        - bg_col : background color.
-        - data : per vertex data for visualization. scalar or vector field.
-        - reset_view : if False view is as left in the previous display call. If
-            True, the view is reset to the default.
-        - once : if True we immediately exit the event loop and return. However,
-            the window stays and if the event loop is called from this or any
-            other viewer, the window will still be responsive.
+            Args:
+            ---
+            - m : the Manifold mesh we are visualizing.
+            - mode : a single character that determines how the mesh is visualized:
+                'w' - wireframe,
+                'i' - isophote,
+                'g' - glazed (try it and see),
+                's' - scalar field,
+                'l' - line field,
+                'n' - normal.
+            - smooth : if True we use vertex normals. Otherwise, face normals.
+            - bg_col : background color.
+            - data : per vertex data for visualization. scalar or vector field.
+            - reset_view : if False view is as left in the previous display call. If
+                True, the view is reset to the default.
+            - once : if True we immediately exit the event loop and return. However,
+                the window stays and if the event loop is called from this or any
+                other viewer, the window will still be responsive.
 
-        Interactive controls:
-        ---
-        When a viewer window is displayed on the screen, you can naviagate with
-        the mouse: Left mouse button rotates, right mouse button is used for
-        zooming and (if shift is pressed) for panning. If you hold control, any
-        mouse button will pick a point on the 3D model. Up to 19 of these points
-        have unique colors.  If you pick an already placed annotation point it
-        will be removed and can now be placed elsewhere. Hit space bar to clear
-        the annotation points. Hitting ESC exits the event loop causing control
-        to return to the script.
-        """
-        data_ct = np.array(data,dtype=ct.c_double).ctypes
-        data_a = data_ct.data_as(ct.POINTER(ct.c_double))
-        bg_col_ct = np.array(bg_col,dtype=ct.c_float).ctypes
-        bg_col_a = bg_col_ct.data_as(ct.POINTER(ct.c_float*3))
-        lib_py_gel.GLManifoldViewer_display(self.obj,m.obj,ct.c_char(mode.encode('ascii')),smooth,bg_col_a,data_a,reset_view,once)
-    def annotation_points(self):
-        """ Retrieve a vector of annotation points. This vector is not a copy,
-        so any changes made to the points will be reflected in the viewer. """
-        pos = ct.POINTER(ct.c_double)()
-        n = lib_py_gel.GLManifoldViewer_get_annotation_points(self.obj, ct.byref(pos))
-        if n == 0:
-            return None
-        return np.ctypeslib.as_array(pos,(n,3))
-    def set_annotation_points(self, pts):
-        n = int(np.size(pts)/3)
-        pts_ct = np.array(pts,dtype=ct.c_double).ctypes
-        pts_a = pts_ct.data_as(ct.POINTER(ct.c_double))
-        lib_py_gel.GLManifoldViewer_set_annotation_points(self.obj, n, pts_a)
-    @staticmethod
-    def event_loop():
-        """ Explicit call to the event loop. This function enters the event loop.
-        Call it if you want to turn on interactivity in the currently displayed
-        window."""
-        lib_py_gel.GLManifoldViewer_event_loop(False)
-
+            Interactive controls:
+            ---
+            When a viewer window is displayed on the screen, you can naviagate with
+            the mouse: Left mouse button rotates, right mouse button is used for
+            zooming and (if shift is pressed) for panning. If you hold control, any
+            mouse button will pick a point on the 3D model. Up to 19 of these points
+            have unique colors.  If you pick an already placed annotation point it
+            will be removed and can now be placed elsewhere. Hit space bar to clear
+            the annotation points. Hitting ESC exits the event loop causing control
+            to return to the script.
+            """
+            data_ct = np.array(data,dtype=ct.c_double).ctypes
+            data_a = data_ct.data_as(ct.POINTER(ct.c_double))
+            bg_col_ct = np.array(bg_col,dtype=ct.c_float).ctypes
+            bg_col_a = bg_col_ct.data_as(ct.POINTER(ct.c_float*3))
+            lib_py_gel.GLManifoldViewer_display(self.obj,m.obj,ct.c_char(mode.encode('ascii')),smooth,bg_col_a,data_a,reset_view,once)
+        def annotation_points(self):
+            """ Retrieve a vector of annotation points. This vector is not a copy,
+            so any changes made to the points will be reflected in the viewer. """
+            pos = ct.POINTER(ct.c_double)()
+            n = lib_py_gel.GLManifoldViewer_get_annotation_points(self.obj, ct.byref(pos))
+            if n == 0:
+                return None
+            return np.ctypeslib.as_array(pos,(n,3))
+        def set_annotation_points(self, pts):
+            n = int(np.size(pts)/3)
+            pts_ct = np.array(pts,dtype=ct.c_double).ctypes
+            pts_a = pts_ct.data_as(ct.POINTER(ct.c_double))
+            lib_py_gel.GLManifoldViewer_set_annotation_points(self.obj, n, pts_a)
+        @staticmethod
+        def event_loop():
+            """ Explicit call to the event loop. This function enters the event loop.
+            Call it if you want to turn on interactivity in the currently displayed
+            window."""
+            lib_py_gel.GLManifoldViewer_event_loop(False)
+except AttributeError:
+    pass
 
 lib_py_gel.MeshDistance_new.restype = ct.c_void_p
 lib_py_gel.MeshDistance_new.argtypes = (ct.c_void_p,)
