3a6479a1586f459f6d86044ae4311a90060f5f2b
Andreas Bærentzen <janba@dtu.dk>
2020-11-19 14:59:51 +0100

    linux tweaks

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ print("Found these libraries: ", libs_data)
 setuptools.setup(
     name="PyGEL3D",
     version="0.0.27",
-    author="Andreas Bærentzen",
+    author="Andreas Baerentzen",
     author_email="janba@dtu.dk",
     description="PyGEL 3D (Python Bindings for GEL) contains tools for polygonal mesh based geometry processing",
     long_description=long_description,
