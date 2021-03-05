2373820d2fc044a07f7b3e461ef6f5a65e080702
timfelle <timfelle@hotmail.com>
2021-03-05 09:46:37 +0100

    Added color capable obj_save

--- src/GEL/HMesh/obj_save.h
+++ src/GEL/HMesh/obj_save.h
@@ -13,12 +13,15 @@
 #define __HMESH_OBJSAVE__H__
 
 #include <string>
+#include <GEL/HMesh/Manifold.h>
 
 namespace HMesh
 {
     class Manifold;
     /// \brief Save in Wavefront OBJ format. 
     bool obj_save(const std::string&, Manifold& m);
+    /// \brief Save in Wavefront OBJ format with color.
+    bool obj_save(const std::string&, Manifold& m, VertexAttributeVector<CGLA::Vec3d> &c);
 
 }
 #endif
