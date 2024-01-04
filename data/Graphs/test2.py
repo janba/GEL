1042e0d199d1a62b203a8c57f7c97016a5282042
Andreas Bærentzen <janba@dtu.dk>
2024-01-04 22:59:07 +0100

    removing some ephemeral files

--- data/Graphs/test2.py
+++
@@ -1,8 +0,0 @@
-from pygel3d import hmesh, graph, gl_display as gl
-m = hmesh.load("as.obj")
-s = graph.LS_skeleton(graph.from_mesh(m))
-f = hmesh.skeleton_to_feq(s)
-#hmesh.cc_split(f)
-#hmesh.fit_mesh_to_ref(f, m)
-v = gl.Viewer()
-v.display(f)
