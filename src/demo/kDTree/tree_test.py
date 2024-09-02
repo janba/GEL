02fea63aa48181060598453fd3481bbd02ee1d76
Andreas Bærentzen <janba@dtu.dk>
2024-09-02 17:22:58 +0200

    little demo of kDTree in Python

---
+++ src/demo/kDTree/tree_test.py
@@ -0,0 +1,27 @@
+from pygel3d import spatial
+from random import uniform
+import time
+
+for trial in range(7):
+    T = spatial.I3DTree()
+
+    print("Building tree")
+    pts = [(uniform(-1, 1), uniform(-1, 1), uniform(-1, 1)) for _ in range(1000000)]
+    for i,p in enumerate(pts):
+        T.insert(p,i)
+    T.build()
+
+    if trial == 0:
+        print("Validating tree")
+        for i,p in enumerate(pts):
+            k, v = T.closest_point(p,1e-6)
+            assert v == i
+
+    print("Timing queries")
+    start_time = time.time()
+    indices = []
+    for p in [(uniform(-1, 1), uniform(-1, 1), uniform(-1, 1)) for _ in range(100000)]: 
+        k, v = T.closest_point(p,1e32)
+        indices.append(k)
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time} seconds")
