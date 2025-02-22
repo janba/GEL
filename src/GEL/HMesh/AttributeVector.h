a84d31e3a1892f690aa39379f908975665f6216e
Andreas Bærentzen <janba@dtu.dk>
2025-02-22 12:18:30 +0100

    Integrate changes from FEQ_from_skeleton for src/GEL/HMesh/AttributeVector.h

--- src/GEL/HMesh/AttributeVector.h
+++ src/GEL/HMesh/AttributeVector.h
@@ -27,6 +27,9 @@ namespace HMesh
     template<typename ITEM, typename ITEMID>
     class AttributeVector
     {
+        std::vector<ITEM> items;
+        ITEM default_value;
+        
     public:
         /// Construct from  size and optional default value (size should be identical to number of entities.
         AttributeVector(size_t _size, ITEM _default_value) :
@@ -36,44 +39,43 @@ namespace HMesh
         /// Construct from optional default value.
         AttributeVector(ITEM _default_value):
         default_value(_default_value) {}
+        
+        /// Return raw pointer to data
+        ITEM* data() {return items.data();}
+        
+        /// Return raw const pointer to data
+        const ITEM* data() const {return items.data();}
 
-        /// const reference to item given by ID
-        const ITEM& get(ITEMID id) const     {
-            assert(id.index < items.size());
-            return items[id.index];
+        /// Returns  value corresponding to index.
+        ITEM operator [](ITEMID idx) const {
+            return items.at(idx.index);
         }
 
-        /// reference to item given by ID
-        ITEM& get(ITEMID id)     {
-            if(id.index >= items.size())
-                items.resize(id.index + 1, default_value);
-            return items[id.index];
+        /// reference to item given by ID. Note that this function may resize the vector.
+        ITEM& operator [](ITEMID idx) {
+            if (idx.index >= items.size())
+                resize(idx.index+1);
+            return items.at(idx.index);
         }
 
-
-        /// const reference to item given by ID
-        const ITEM& operator [](ITEMID id) const {
-            return get(id);
-        }
-
-
-        /// reference to item given by ID
-        ITEM& operator [](ITEMID id) {
-            return get(id);
-        }
-
-        /// resize the vector (may be necessary if associated container size grows)
-        void resize(size_t _size, ITEM _default_value = ITEM()) {
+        /** resize the vector (may be necessary if associated container size grows). This
+         version of the function takes a new default value as argument. This will override and
+         overwrite the previous default value. Rarely useful since the default value can be set
+         in the constructor. */
+        void resize(size_t _size, ITEM _default_value) {
             default_value = _default_value;
             items.resize(_size, default_value);
         }
 
+        /// resize the vector (may be necessary if associated container size grows)
+        void resize(size_t _size) {
+            items.resize(_size, default_value);
+        }
 
         /// number of attribute items in vector
         size_t size() const {
             return items.size();
         }
-
 
         /// clear the vector
         void clear() {
@@ -101,10 +103,6 @@ namespace HMesh
             ser.read(default_value);
         }
 
-
-    private:
-        std::vector<ITEM> items;
-        ITEM default_value;
     };
 
     template<typename ITEM>
