4d695f3749a85614d910ec576404f7c8e9b5c803
Andreas Bærentzen <janba@dtu.dk>
2025-07-02 22:06:21 +0200

    In C++ it is true, not True

--- src/GEL/HMesh/RsR.cpp
+++ src/GEL/HMesh/RsR.cpp
@@ -816,7 +816,7 @@ void minimum_spanning_tree(const SimpGraph& g, NodeID root,
         gn.add_node(vertices[n], normals[n]);
 
     Util::AttribVec<NodeID, unsigned char> in_tree(gn.no_nodes(), false);
-    in_tree[root] = True;
+    in_tree[root] = true;
 
     std::priority_queue<QElem> Q;
     for (auto n : g.neighbors(root)) {
