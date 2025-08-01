a07187fc3e5b64b5cc83afa26c1983cb4b27b818
Cem Akarsubasi <cemakarsubasi@gmail.com>
2025-08-01 10:55:34 +0200

    Template estimateNormal so I can call it without creating a temporary vector.

--- src/GEL/Geometry/normal.h
+++ src/GEL/Geometry/normal.h
@@ -1,16 +1,36 @@
-#ifndef CGLA_Normal_h
-#define CGLA_Normal_h
+#ifndef GEOMETRY_NORMAL_H
+#define GEOMETRY_NORMAL_H
 #pragma once
 
-#include <GEL/CGLA/Definitions.h>
-#include <vector>
+#include <GEL/CGLA/Vec3d.h>
+#include <GEL/CGLA/Mat3x3d.h>
 
-using namespace CGLA;
+namespace Geometry
+{
 
-namespace Geometry {
-	Vec3d estimateNormal(const std::vector<Vec3d>& neighbors);
+CGLA::Vec3d smallestEigenVector(const CGLA::Mat3x3d& matrix);
 
-	Vec3f smallestEigenVector(const Mat3x3f& matrix);
+/// Given an input range of coordinates, estimate a normal vector
+template <std::ranges::input_range Range>
+CGLA::Vec3d estimateNormal(Range&& neighbors)
+{
+    CGLA::Vec3d centroid(0.0f);
+    for (const auto& point : neighbors) {
+        centroid += point;
+    }
+    centroid /= static_cast<double>(neighbors.size());
+
+    CGLA::Mat3x3d covariance(0.0f);
+    for (const auto& point : neighbors) {
+        CGLA::Vec3d diff = point - centroid;
+        covariance += outer_product(diff, diff);
+    }
+    covariance /= static_cast<double>(neighbors.size());
+
+    const CGLA::Vec3d normal = smallestEigenVector(covariance);
+
+    return normalize(normal); // Normalize to ensure unit length
+}
 }
 
 #endif
